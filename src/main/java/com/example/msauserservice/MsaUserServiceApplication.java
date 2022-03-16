ae41d0ed32c7e8f590856533d68b4b7183245fdf
chaenabi <find1086@gmail.com>
2022-03-16 17:47:26 +0900

    first commit

---
+++ src/main/java/com/example/msauserservice/MsaUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msauserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msauserservice/MsaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msauserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
